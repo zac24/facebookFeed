a6ced5dcf90738caf8b59bd6491b9eeafa2cf2be
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-01-27 14:58:37 +0530

    navigation bar color and appearance changes

--- facebookFeed/AppDelegate.swift
+++ facebookFeed/AppDelegate.swift
@@ -13,9 +13,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let feedController = FeedController(collectionViewLayout: UICollectionViewFlowLayout())
+        let navigationController = UINavigationController(rootViewController:feedController)
+        window?.rootViewController = navigationController
+        
+        
+        UINavigationBar.appearance().barTintColor =  UIColor(red: 51/255, green: 90/255, blue: 149/255, alpha: 1.0)
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
+        
+        application.statusBarStyle = .lightContent
+        
+        
         return true
     }
 
--- facebookFeed/ViewController.swift
+++ facebookFeed/ViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let cellId = "cellId"
+class FeedController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "Facebook Feed"
+        
+        collectionView?.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
+        
+        collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+}
 
 
+class FeedCell: UICollectionViewCell {
+    
+    override init(frame :CGRect){
+        super.init(frame:frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.white
+    }
+    
 }
 
