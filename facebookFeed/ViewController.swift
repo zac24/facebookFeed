65c42768fd1b2811fb717debbc6032d11f5070f4
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-01-27 09:57:43 +0530

    first commit for facebookfeed

---
+++ facebookFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  facebookFeed
+//
+//  Created by Prashant Dwivedi on 1/25/18.
+//  Copyright © 2018 Prashant Dwivedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
