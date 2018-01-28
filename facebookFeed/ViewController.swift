f30dabcc243593aa4f3ac0ac91d9f6b186793c5e
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-01-28 20:07:43 +0530

    added new artifacts and cells size according to their dynamic content

--- facebookFeed/ViewController.swift
+++ facebookFeed/ViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 let cellId = "cellId"
 
 class Post  {
-    var name : String?
+    var name: String?
+    var profileImageName: String?
+    var statusText: String?
+    var statusImageName: String?
+    var numLikes: NSNumber?
+    var numComments: NSNumber?
 }
 
 class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
@@ -21,15 +26,63 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+//
+//        let postMark = Post()
+//        postMark.name = "Mark Zuckerberg"
+//        postMark.statusText = "Meanwhile, Beast turned to the dark side."
+//        postMark.profileImageName = "zuckprofile"
+//
+//        let postSteve = Post()
+//        postSteve.name = "Steve Jobs"
+//        postSteve.statusText = "Design is not just what it looks like and feels like. Design is how it works.\n\n" + "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me.\n\n" + "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
+//        postSteve.profileImageName = "steve_profile"
+//
+//        posts.append(postMark)
+//        posts.append(postSteve)
+//
+        
         
         let postMark = Post()
         postMark.name = "Mark Zuckerberg"
+//        postMark.location = Location()
+//        postMark.location?.city = "San Francisco"
+//        postMark.location?.state = "CA"
+        postMark.profileImageName = "zuckprofile"
+        postMark.statusText = "By giving people the power to share, we're making the world more transparent."
+        postMark.statusImageName = "zuckdog"
+        postMark.numLikes = 400
+        postMark.numComments = 123
         
         let postSteve = Post()
         postSteve.name = "Steve Jobs"
+//        postSteve.location = Location()
+//        postSteve.location?.city = "Cupertino"
+//        postSteve.location?.state = "CA"
+        postSteve.profileImageName = "steve_profile"
+        postSteve.statusText = "Design is not just what it looks like and feels like. Design is how it works.\n\n" +
+            "Being the richest man in the cemetery doesn't matter to me. Going to bed at night saying we've done something wonderful, that's what matters to me.\n\n" +
+        "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations."
+        postSteve.statusImageName = "steve_status"
+        postSteve.numLikes = 1000
+        postSteve.numComments = 55
+        
+        let postGandhi = Post()
+        postGandhi.name = "Mahatma Gandhi"
+//        postGandhi.location = Location()
+//        postGandhi.location?.city = "Porbandar"
+//        postGandhi.location?.state = "India"
+        postGandhi.profileImageName = "gandhi_profile"
+        postGandhi.statusText = "Live as if you were to die tomorrow; learn as if you were to live forever.\n" +
+            "The weak can never forgive. Forgiveness is the attribute of the strong.\n" +
+        "Happiness is when what you think, what you say, and what you do are in harmony."
+        postGandhi.statusImageName = "gandhi_status"
+        postGandhi.numLikes = 333
+        postGandhi.numComments = 22
+        
         
         posts.append(postMark)
         posts.append(postSteve)
+        posts.append(postGandhi)
         
         navigationItem.title = "Facebook Feed"
         
@@ -55,7 +108,15 @@ class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLa
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
                         sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = CGSize(width:view.frame.width, height:400)
+        
+        if let statusText = posts[indexPath.item].statusText {
+            let statusSize = CGSize(width:view.frame.width, height:1000)
+            let rect = NSString(string:statusText).boundingRect(with: statusSize, options: NSStringDrawingOptions.usesFontLeading.union(NSStringDrawingOptions.usesLineFragmentOrigin), attributes: [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 14)], context:nil)
+            let knownHeight : CGFloat = 8 + 44 + 4 + 4 + 200 + 8 + 24 + 8 + 44
+            return CGSize(width: view.frame.width, height: rect.height + knownHeight + 18)
+        }
+        
+        let size = CGSize(width:view.frame.width, height:500)
 //        print(size)
         return size
     }
@@ -82,6 +143,18 @@ class FeedCell: UICollectionViewCell {
             
             nameLabel.attributedText = attributedText
             }
+            
+            if let statusText = post?.statusText {
+                statusTextView.text = statusText;
+            }
+            
+            if let profileImageName = post?.profileImageName {
+                profileImageView.image = UIImage(named:profileImageName)
+            }
+            
+            if let statusImageName = post?.statusImageName {
+                statusImageView.image = UIImage(named:statusImageName)
+            }
         }
     }
     
@@ -107,6 +180,7 @@ class FeedCell: UICollectionViewCell {
         let textView = UITextView()
         textView.text = "Meanwhile, Beast turned to the dark side."
         textView.font = UIFont.systemFont(ofSize: 14)
+        textView.isScrollEnabled = false
         return textView
     }()
     
@@ -177,7 +251,7 @@ class FeedCell: UICollectionViewCell {
         addConstraintsWithFormat(format: "H:|-12-[v0]-12-|", views: devideLineView)
         addConstraintsWithFormat(format: "H:|[v0(v2)][v1(v2)][v2]|", views: likeButton, commentButton, shareButton)
         addConstraintsWithFormat(format: "V:|-12-[v0]", views: nameLabel)
-        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(1)][v5(44)]|", views: profileImageView,statusTextView,statusImageView,likeCommentLabel,devideLineView,likeButton)
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]-4-[v1]-4-[v2(200)]-8-[v3(24)]-8-[v4(1)][v5(44)]|", views: profileImageView,statusTextView,statusImageView,likeCommentLabel,devideLineView,likeButton)
         addConstraintsWithFormat(format: "V:[v0(44)]|", views: commentButton)
         addConstraintsWithFormat(format: "V:[v0(44)]|", views: shareButton)
         
