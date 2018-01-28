aa42a26a1272d0030cf23186408d8bdf027d3bc3
Prashant Dwivedi <user@jios-MacBook-Pro.local>
2018-01-28 10:28:09 +0530

    added assets and visual format constraints

--- facebookFeed/ViewController.swift
+++ facebookFeed/ViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 let cellId = "cellId"
-class FeedController: UICollectionViewController {
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         navigationItem.title = "Facebook Feed"
+        
+        collectionView?.alwaysBounceVertical = true
         
         collectionView?.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
         
@@ -29,6 +31,14 @@ class FeedController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGSize(width:view.frame.width, height:60)
+//        print(size)
+        return size
     }
 }
 
@@ -45,9 +55,66 @@ class FeedCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Mark Zuckerberg", attributes:[NSAttributedStringKey.font:UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string:"\nDecember 18  •  San Francisco  • ", attributes: [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 12), NSAttributedStringKey.foregroundColor:UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)]))
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 4
+        
+        attributedText.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.string.count))
+        
+        label.attributedText = attributedText
+        
+        return label
+    }()
+    
+    let profileImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named:"zuckprofile")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+        
+    }()
+    
     func setupViews() {
         backgroundColor = UIColor.white
+        
+        addSubview(nameLabel)
+        addSubview(profileImageView)
+        
+        addConstraintsWithFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstraintsWithFormat(format: "V:|-8-[v0(44)]", views: profileImageView)
+        
     }
     
 }
+
+extension UIView {
+    
+    func addConstraintsWithFormat(format: String, views:UIView...) {
+        var viewsDictionary = [String:UIView]()
+        for (index, view) in views.enumerated(){
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
+
+
+
+
+
+
+
+
 
